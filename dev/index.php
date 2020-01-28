6dd0881e96a2a675560025d9c9e35520a88f47db
simeon aleksandrov <globaxy.simeon@gmail.com>
2020-01-28 10:26:15 +0200

    update

--- dev/index.php
+++ dev/index.php
@@ -1,16 +1,51 @@
-<?php include 'includes/header.php' ?>
-<main class="main-content">
-	<h1>Main Title</h1>
-	<h2>Sub Title</h2>
-    <h3>Small Title</h3>
-	<ul>
-		<li class="with">item1</li>
-		<li class="with">item2</li>
-        <li class="with">item3</li>
-	</ul>		
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>learn sass</title>
+        <meta name="keywords" content=""/>
+        <meta name="description" content=""/>
+        <meta name="viewport" content="width=device-width, initial-scale=1"/>
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,700&display=swap&subset=greek" rel="stylesheet">
+        <link rel="icon" type="image/jpg" href="favicon.jpg">
+        <!--build:css css/styles.min.css-->
+        <link rel="stylesheet" href="css/main1.css">
+        <link rel="stylesheet" href="css/main2.css">
+        <!--endbuild-->
+    </head>
+<body>
+    <div class="site">
+        <header class="main-header">
+            <a href="" title="link1">link1</a>
+            <a href="" title="link2">link2</a>
+        </header>
 
-	<div class="text">Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.</div>
-</main>
-<?php include 'includes/footer.php' ?>
+		<main class="main-content">
+			<h1>Main Text</h1>
+			<h2>Sub Text</h2>
+            <h3>Small text</h3>
+			<ul>
+				<li class="with">item1</li>
+				<li class="with">item2</li>
+                <li class="with">item3</li>
+                <li class="with">item4</li>
+			</ul>		
+
+			<div class="text">Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.</div>
+            <div class="text">Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.</div>
+		</main>
+		<footer class="main-footer">
+		    <p>footer</p>
+		    <div>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</div>
+		</footer>
+	</div>    
+<!--build:js js/main.min.js -->
+<script src="js/plugins/plugin1.js"></script>
+<script src="js/plugins/plugin2.js"></script>
+<script src="js/main.js"></script>
+<!-- endbuild -->
+</body>
+</html>
 
 
